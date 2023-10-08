6b86f50086987ba8923bc8dd7322d0cbf0c19702
ayoublatreb <ayoublatreb@gmail.com>
2023-10-08 16:28:13 +0200

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx if it's not already installed
-if ! dpkg -l | grep -q nginx; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create or recreate the symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Give ownership of the /data/ folder to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
 
-# Update Nginx configuration
-config_content="location /hbnb_static {
-    alias /data/web_static/current;
-}
-"
+    location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
 
-sudo sh -c "echo \"$config_content\" > /etc/nginx/sites-available/default"
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Restart Nginx
-sudo service nginx restart
+service nginx restart
