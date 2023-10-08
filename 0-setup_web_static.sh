2a436edcd0fe2bdf7fb734ea2902c62761615732
ayoublatreb <ayoublatreb@gmail.com>
2023-10-08 16:13:24 +0200

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,37 @@
-#!/usr/bin/env bash
-# script to configure in order to serve the static pages
-apt-get -y update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-# instructions
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+#!/bin/bash
+
+# Install Nginx if it's not already installed
+if ! dpkg -l | grep -q nginx; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
 echo "<html>
   <head>
   </head>
   <body>
-    Fake content
+    Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-# symbolic link to folder erase
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# change owner and group to ubuntu
-chown -R ubuntu:ubuntu /data/
-# change content of available-default
-sed -i '43i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t autoindex on;\n\t}\n' /etc/nginx/sites-available/default
-# getting changes
-service nginx restart
-exit 0
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Create or recreate the symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config_content="location /hbnb_static {
+    alias /data/web_static/current;
+}
+"
+
+sudo sh -c "echo \"$config_content\" > /etc/nginx/sites-available/default"
+
+# Restart Nginx
+sudo service nginx restart
